38f9202b4b91689b32884a4b5f61a9d10b9a6fba
Blaze <51222153+13laze@users.noreply.github.com>
2019-05-31 21:22:36 -0700

    Create example.c

---
+++ example.c
@@ -0,0 +1,20 @@
+#include "pch.h" //Dont include if you are not using Visual Studio
+#include "memwrapper.h"
+
+using namespace MemoryWrapper; //other you will have to write MemoryWrapper::Memory
+
+int main()
+{
+  Memory m;
+  m.gameTitle = "Geometry Dash";
+  DWORD processID = m.GetPID();
+  
+  DWORD base = m.GetModuleBaseAddress(processID, L"GeometryDash.exe");
+  DWORD offset = (DWORD) 0xAE8FA;
+  
+  BYTE byte = 0xEB;
+  m.WriteToMemory((BYTE*)byte, (base+offset), 1); // We cast byte to a byte pointer because WriteToMemory takes a byte array
+
+  //No need to do 'delete m' because m is not a pointer, it gets popped off the stack when finished
+  return 0;
+}
